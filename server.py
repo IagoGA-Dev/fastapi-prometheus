f75798dd0d2bb58a13eb728d2f4337dfb07e0064
Iago Araujo <iagogaraujo@gmail.com>
2024-09-25 16:28:41 -0300

    Implements prometheus

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import random
 
 from fastapi import FastAPI, HTTPException, Response
 from fastapi.middleware.cors import CORSMiddleware
+import prometheus_client
 import uvicorn
 
 app = FastAPI()
@@ -11,6 +12,21 @@ app.add_middleware(
     allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
+)
+
+head_count = prometheus_client.Counter(
+    "head_count",
+    "Number of heads"
+)
+
+tails_count = prometheus_client.Counter(
+    "tails_count",
+    "Number of tails"
+)
+
+total_flips = prometheus_client.Counter(
+    "total_flips",
+    "Total number of flips"
 )
 
 @app.get("/flip-coins")
@@ -25,10 +41,18 @@ async def flip_coins(times=None):
     
     tails = times_as_int - heads
 
+    head_count.inc(heads)
+    tails_count.inc(tails)
+    total_flips.inc(times_as_int)
+
     return {
         "heads": heads,
         "tails": tails
     }
+
+@app.get("/metrics")
+async def metrics():
+    return Response(prometheus_client.generate_latest())
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
